79db42c94005bb989141d130ba69d4ba7b16a4fc
bitwitch <shawwian@gmail.com>
2019-01-19 18:30:00 -0500

    implement brackets

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h> 
 
-/* filecopy: copy file ifp to file ofp */
+// filecopy: copy file ifp to file ofp
 void filecopy(FILE *ifp, FILE *ofp)
 {
 	int c; 
@@ -12,16 +12,15 @@ int main(int argc, char** argv)
 {
 	// memory
 	int MEMORY_MAX = 30000;
+	int CODE_MAX = 30000;
 	int memory[MEMORY_MAX];
+	char code[CODE_MAX];
 	int *ptr = memory;
-	FILE* l_stack[MEMORY_MAX];
-	int ls_index = 0;
-	int c;
+	int c, i, loop;
 
 	// initialize memory and bracket stack
 	for(int i=0; i<MEMORY_MAX; i++) {
 		memory[i]  = 0;
-		l_stack[i] = 0; 
 	}
 
 	printf("brainfuck\n");
@@ -34,112 +33,65 @@ int main(int argc, char** argv)
 			printf("Error: cannot open file %s\n", argv[1]);
 			return 1;
 		}
-
-		FILE *code = tmpfile();
-		if (code == NULL) { 
-			puts("Unable to create temp file bf_program");
-			return 1; 
-		} 
-	
+		
 		// parse the file to strip whitespace and bullshit
-		while ((c = getc(fp)) != EOF) {
+		for (i = 0; (c = getc(fp)) != EOF; ) {
 			if (c == '+' || c == '-' || c == '>' || c == '<' || 
 				c == '.' || c == ',' || c == '[' || c == ']')
 			{
-				putc(c, code);
+				code[i++] = c;	
 			}
 		}
-
+        
 		fclose(fp);
-		rewind(code);
+		code[i] = '\0';
 
-		// parse the copied version of the program
-		while ((c = fgetc(code)) != EOF) {
-			switch(c) {	
-				case '+': ++*ptr; break;
-				case '-': --*ptr; break;
-				case '>': ++ptr; break;	
-				case '<': --ptr; break;
-				case '.': putchar(*ptr); break;
-				case '[':  
-					if (*ptr == 0) {
-						// jump to matching ] 
-						
-						
-						while ((c = fgetc(code)) != EOF) {
-							if (c == '[') { 
-
-								l_stack[ls_index++] = code;
-								
-							} else if (c == ']') {
-
-								if (ls_index == 0) {
-									fgetc(code);
-								} else {
-
-								}
-
-							}
-						}
-
-
-					} else {
-						// push the current brackets file location on a stack and continue
-						l_stack[ls_index++] = code;
-					}
-					break;	
-				case ']':    
-					if (*ptr == 0) {
-						// pop from the left bracket stack and jump to that brackets position
-						FILE* file_pos = l_stack[ls_index--];
-						fseek(code, -2, SEEK_CUR);
-					} break;
-				default: 
-					break;
-			}
-		}
-
-		return 0;
-
-	} else { // no file given as argument
-
-		int c; 
-		while ( (c = getchar()) != EOF ) 
-		{
-			switch(c) 
-			{	
-				case '+': ++*ptr; break;
-				case '-': --*ptr; break;
-				case '>': ++ptr; break;
-				case '<': --ptr; break;
-				case '.': putchar(*ptr); break;
-					// if (*ptr >= 32 && *ptr <= 126) {
-					// 	printf("%c", *ptr);
-					// } else {
-					// 	printf("%i", *ptr);
-					// }
-					// break;
-				case '[':    
-					if (*ptr == 0) {
-						// jump to closing bracket
-						while( (c = getchar()) != ']')
-							;
-					} 
-					break;
-					
-				case ']':    
-					if (*ptr == 0) {
-						// jump to opening bracket
-					} 
-					break;
-				default: 
-					break;
-			}
-		}
-			
-		putchar('\n'); 
-
-
-		return 0; 
+	} else { 
+		// read brainfuck program from stdin 
+		fgets(code, CODE_MAX, stdin);	
 	}
+	
+	// interpret the program
+	for (i = 0; code[i] != 0; i++) {
+		char c = code[i];
+		switch(c) {	
+			case '+': ++*ptr; break;
+			case '-': --*ptr; break;
+			case '>': ++ptr; break;	
+			case '<': --ptr; break;
+			case '.': putchar(*ptr); break;
+			case '[':  
+				if (*ptr == 0) {
+					// jump to matching ] 
+					loop = 1; 
+					while (loop > 0) { 
+						c = code[++i]; 
+						if (c == '[') { 
+							loop++; 
+						} else if (c == ']') { 
+							loop--; 
+						} 
+					} 
+				} 
+				break;	
+			case ']':
+				if (*ptr != 0) {   
+					loop = 1;
+					while (loop > 0) {
+						c = code[--i];
+						if (c == '[') {
+							loop--;
+						} else if (c == ']') {
+							loop++;
+						}
+					}
+				}
+				break;
+			default: 
+				break;
+		}
+	}
+		
+	putchar('\n'); 
+	return 0;
 }
